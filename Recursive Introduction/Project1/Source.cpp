de7ec19d3f5420ace5a308033b3d888bc64975fc
DevonWenYan <devonproverb@gmail.com>
2016-11-08 00:48:05 +0800

    Recursive Introduction
    Recursive Introduction

---
+++ Recursive Introduction/Project1/Source.cpp
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int factorial(int n)
+{
+	if (n == 1)
+		return(1);
+	else
+		return(n*factorial(n - 1));
+}
+
+
+void main(void)
+{
+	int i;
+	printf("¶}©l»¼°j:\n");
+	for (i = 5; i >0; i--)
+	{
+		printf("%d! = %d\n", i, factorial(i));
+	}
+	system("pause");
+	return;
+}
